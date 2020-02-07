89e43b61861a9fd00bd276289372df512d84d67b
thaisrfernandes <thais.fernandes@ilegra.com>
2020-02-07 10:50:46 -0300

    [remove] comments

--- src/App.js
+++ src/App.js
@@ -39,31 +39,4 @@ export default function App() {
       ))}
     </ul>
   );
-
-  /*
-  const [repositories, setRepositories] = useState([[
-    { id: 1, name: "repo-1" },
-    { id: 2, name: "repo-2" },
-    { id: 3, name: "repo-3" }
-  ])];
-  
-  function handleAddRepository() {
-    setRepositories([
-      ...repositories,
-      { id: Math.random(), name: "Novo repo" }
-    ]);
-  }
-
-  return (
-    <>
-      <ul>
-        {repositories.map(repo => (
-          <li key={repo.id}>{repo.name}</li>
-        ))}
-      </ul>
-      <button onClick={handleAddRepository}>
-        Adicionar Repositório
-      </button>
-    </>
-  );*/
 }
